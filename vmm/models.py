d3e9874baeaa15e2608266ed1c786e9f34213ec8
Liuzhongqi <myft_code@163.com>
2017-08-22 15:31:02 +0800

    调整数据表！

--- vmm/models.py
+++ vmm/models.py
@@ -12,8 +12,8 @@ class users(models.Model):
     real_name = models.CharField(max_length=20)
     user_id = models.CharField(max_length=20, primary_key=True)
     user_password = models.CharField(max_length=20)
-    email = models.EmailField(max_length=40, primary_key=True)
-    mobile = models.CharField(blank=True)
+    email = models.EmailField(max_length=40)
+    mobile = models.CharField(max_length=11,blank=True)
     isadmin = models.BooleanField(default=False)
     comment = models.TextField(blank=True)
 
@@ -23,17 +23,17 @@ class users(models.Model):
 
 # 创建虚拟机信息表
 class vms(models.Model):
-    vm_name = models.CharField(max_length=50, primary_key=True)
+    vm_name = models.CharField(max_length=50)
     vm_uuid = models.CharField(max_length=50, primary_key=True)
     vm_purpose = models.TextField()
     vm_comment = models.TextField()
-    vm_os_admin = models.CharField(max_length=20)
-    vm_os_pass = models.CharField(blank=True)
-    vm_owner = models.CharField(max_length=20)
+    vm_os_admin = models.IntegerField()
+    vm_os_password = models.CharField(max_length=20,blank=True)
+    vm_user_id = models.CharField(max_length=20)
     vm_type = models.IntegerField()
     vm_ports = models.CharField(max_length=20)
 
-    vm_ip = models.CharField(max_length=15, db_index=True)
+    vm_ip = models.CharField(max_length=20)
     vm_os = models.CharField(max_length=20)
     vm_cpu = models.IntegerField()
     vm_memory = models.IntegerField()
