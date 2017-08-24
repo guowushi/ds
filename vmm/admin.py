fe5f3d7da9dbbbc64dae8ac57f299e95da60518f
xiaoxin <1257998975@qq.com>
2017-08-24 16:57:23 +0800

    增加了显示使用者，修改了开关机

--- vmm/admin.py
+++ vmm/admin.py
@@ -15,7 +15,7 @@ from pyVmomi import vim
 
 # 引用模型和表单
 from vmm.models import users
-
+from vmm.models import vms
 
 '''
 定义一个方法，处理用户的管理员登录！
@@ -46,11 +46,20 @@ def listvm(request):
             viewType = [vim.VirtualMachine]  # 查找的对象类型是什么
             recursive = True  # 是否进行递归查找
             containerView = content.viewManager.CreateContainerView(container, viewType, recursive)
-            vms = containerView.view  # 执行查找
-
+            vmss = containerView.view  # 执行查找
+            vm_infor = vms.objects.all() # 获得vms表单信息
+            user_info = users.objects.all() # 获得users表单信息
+            vms2 = []                      # 用于打包虚拟机列表及其使用者
+            for vm in vmss:
+                for vm2 in vm_infor:
+                    if(vm.summary.config.uuid==vm2.vm_uuid):  #遍历数据库找到对应虚拟机使用者
+                        for user in user_info:
+                            if(user.user_id==vm2.vm_user_id):
+                                vms2.append(vm+user.real_name)
+                                break;
             # 载入模板，传递一个集合给模板，让模板渲染成html返回
             tp = loader.get_template("backend/list.html")
-            html = tp.render({"vms": vms})
+            html = tp.render({"vms": vms2})
             return HttpResponse(html)
         except vmodl.MethodFault as error:
             return HttpResponse("Caught vmodl fault : " + error.msg)
@@ -58,3 +67,12 @@ def listvm(request):
         tp = loader.get_template("backend/list.html")
         html = tp.render()
         return HttpResponse("页面未找到！")
+
+
+#连到数据库获取所有信息
+def sql(request):
+    vm_infor=vms.objects.all()
+    db_info = users.objects.all()
+    for dd in db_info:
+        if(dd.user_id=="001"):
+            return HttpResponse(dd.user_password)
